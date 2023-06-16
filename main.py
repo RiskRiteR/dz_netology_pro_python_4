6ff311e6080e7c0d8466c610b2330e884b2ff529
RiskRiteR <kazimir.vm@gmail.ru>
2023-06-16 16:37:03 +0300

    additional task

--- main.py
+++ main.py
@@ -65,6 +65,73 @@ def test_2():
     assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
 
 
+class FlatIterator2:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = list(reversed(list_of_list))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.list_of_list:
+            self.chunk = self.list_of_list.pop()
+            if type(self.chunk) is not list:
+                return self.chunk
+            else:
+                for el in reversed(self.chunk):
+                    self.list_of_list.append(el)
+        raise StopIteration
+
+
+def test_3():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator2(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator2(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+
+def flat_generator_2(list_of_list):
+    for value in list_of_list:
+        if type(value) is list:
+            yield from flat_generator_2(value)
+        else:
+            yield value
+
+
+def test_4():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator_2(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator_2(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator_2(list_of_lists_2), types.GeneratorType)
+
+
 if __name__ == '__main__':
     test_1()
     test_2()
+    test_3()
+    test_4()
